bba27ade599714a63371e553fd7d7562c3b7af74
Akakatsu34 <greenpigeon0304@gmail.com>
2022-12-03 02:32:46 +0900

    詳細画面の実装　完成

--- app/controllers/corporations_controller.rb
+++ app/controllers/corporations_controller.rb
@@ -6,15 +6,23 @@ class CorporationsController < ApplicationController
     @bs = Bs.all
     @pls = Pl.all
     @cfs = Cf.all
-    @bs_asset_sum = Bs.all.sum(:asset)
+    @bs_asset_sum = Corporation.all.sum(:asset)
     @bs_net_asset_sum = Bs.all.sum(:net_asset)
     @corporation_data = Corporation.where(security_code: 1301)
   end
 
   def show
-    @corporation_results = @q.result
-    @corporation_name = Corporation.find_by(security_code: 9684)
-    @corporation_asset = Bs.find_by(asset: @corporation_name.security_code)
+    if params[:q] != nil
+      params[:q]['security_name_cont_any'] = params[:q]['security_name_cont_any'].split(/[\p{blank}\s]+/)
+      @q = Corporation.ransack(params[:q])
+      @q.sorts = 'net_sale desc' if @q.sorts.empty?
+      @corporation_results = @q.result
+    else
+      @q = Corporation.ransack(params[:q])
+      @q.sorts = 'net_sale desc' if @q.sorts.empty?
+      @corporation_results = @q.result #検索の結果を受け取る。
+    end
+    @count = 50
   end
 
   def search
